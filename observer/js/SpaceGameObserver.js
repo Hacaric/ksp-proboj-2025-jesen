e983db4fc54da475be2ea307cb86cd8ee013ac69
hacaric <hacaric.dev@gmail.com>
2025-12-29 21:39:11 +0100

    Optimalization: skip rendering when game is paused - rendering is now skipped if game is paused and camera hasn't moved - camera now operates on two decimal places - assested by Gemini

--- observer/js/SpaceGameObserver.js
+++ observer/js/SpaceGameObserver.js
@@ -9,6 +9,8 @@ class SpaceGameObserver {
         this.inputHandler = new InputHandler(this.canvas, this.camera, this);
         this.timelineManager = new TimelineManager(this, this.dataManager, redirectUrl);
 
+        this.old_camera_footprint = null;
+        this.firstFrame = true;
         this.init();
     }
 
@@ -28,11 +30,21 @@ class SpaceGameObserver {
 
     animate() {
         this.camera.update();
+        
+        let camera_data = this.camera.getDataFootPrint();
+        let camera_moved = (camera_data != this.old_camera_footprint);
+        this.old_camera_footprint = camera_data
 
-        this.renderer.gameData = this.dataManager.getGameData();
-        this.renderer.selectedEntity = this.dataManager.getInterpolatedSelectedEntity();
-        this.renderer.render();
-
+        let should_update = this.firstFrame || (!this.timelineManager.isPaused) || camera_moved;
+        if (should_update){
+            this.renderer.render();
+            this.renderer.gameData = this.dataManager.getGameData();
+            this.renderer.selectedEntity = this.dataManager.getInterpolatedSelectedEntity();
+        } else {
+            // console.log("Skipping rendering...");
+        }
+        
+        this.firstFrame = false;
         this.animationFrameId = requestAnimationFrame(() => this.animate());
     }
 }
